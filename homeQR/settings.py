9f83da783abf4b30d8a6eed99cdf44412cf98f1a
Phil <philconow@gmail.com>
2020-11-04 14:15:45 -0800

    cleanup

--- homeQR/settings.py
+++ homeQR/settings.py
@@ -78,9 +78,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                
-                # My Templates
-                #'items.context_processors.menu_rooms',
             ],
         },
     },
--- items/context_processors.py
+++
@@ -1,6 +0,0 @@
-from .models import Room
-
-def menu_rooms(request):
-     rooms = Room.objects.all().order_by("name")
-    
-     return {'menu_rooms': rooms}
