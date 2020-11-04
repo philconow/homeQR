5d188a5e36a05e6d1fca2dc52f3e97404e6b218a
Phil <philconow@gmail.com>
2020-11-04 10:56:16 -0800

    cleanup

--- QR/t.py
+++
@@ -1,5 +0,0 @@
-from segno import make_qr
-
-qr_code = make_qr('HELLO')
-
-print(type(qr_code))
--- QR/views.py
+++ QR/views.py
@@ -5,10 +5,6 @@ from QR.models import QRBlock
 
 from .forms import AddQRBlockForm
 
-def qr_block_list(request):
-    qrblocks = QRBlock.objects.all()
-    return render(request, 'qr_block_list.html', {'qrblocks', qrblocks})
-
 @login_required
 def add_qr_block(request):
     if request.method == 'POST':
@@ -17,8 +13,7 @@ def add_qr_block(request):
             qr_block = form.save(commit=False)
             qr_block.created_by = request.user
             qr_block.save()
-            return redirect('dashboard.html')
+            return redirect('dashboard')
     else:
-        form = AddQRBlockForm()
-    
+        form = AddQRBlockForm()    
     return render(request, 'add_qr_block.html', {'form': form})
