2e99015dc18bffc9ac0b83e1a50fc3cfe35d31ed
Phil <philconow@gmail.com>
2020-11-05 01:09:42 -0800

    cleaned up

--- homeQR/settings.py
+++ homeQR/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY_HIDDEN
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'frontpage'
