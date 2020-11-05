3dfe9cf79160f3e54aac3c8a077618b55086d3d6
Phil <philconow@gmail.com>
2020-11-05 01:10:27 -0800

    adding room_list view and html

--- homeQR/urls.py
+++ homeQR/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 from django.contrib.auth import views
 
 from core.views import frontpage, about, contact, signup
-from items.views import container_detail, location_detail, room_detail
+from items.views import container_detail, location_detail, room_detail, room_list
 from QR.views import add_qr_block, view_qr_block, delete_qr_block, view_qr_code
 from userprofile.views import dashboard
 
@@ -42,6 +42,7 @@ urlpatterns = [
     path('qr/block/delete/<int:qrblock_id>/', delete_qr_block, name='delete_qr_block'),
     path('qr/code/view/<int:qr_id>/', view_qr_code, name='view_qr_code'),
     
+    path('rooms/', room_list, name='room_list'),
     path('<slug:slug>/', room_detail, name='room_detail'), 
     path('<slug:room_slug>/<slug:slug>/', location_detail, name='location_detail'),  
     path('<slug:room_slug>/<slug:location_slug>/<slug:slug>/', container_detail, name='container_detail'),    
--- items/views.py
+++ items/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Container, Location, Room
 
-# Create your views here.
-
 def container_detail(request, room_slug, location_slug, slug):
     container = get_object_or_404(Container, slug=slug)
     context = { 
@@ -29,5 +27,8 @@ def room_detail(request, slug):
         'room': room,
         'locations': locations
     }
-
     return render(request, 'room_detail.html', context)
+
+def room_list(request):
+    rooms = Room.objects.all()
+    return render(request, 'room_list.html', {'rooms': rooms})
