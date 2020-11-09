fda9ff0d8167f2d4db571fee8c297bc1dde7930e
Phil <philconow@gmail.com>
2020-11-09 14:43:20 -0800

    removed foreignkey fields and added image fields

--- items/forms.py
+++ items/forms.py
@@ -1,21 +1,23 @@
 from django import forms
-
 from .models import Item, Container, Location, Room
 
 class AddItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['name', 'container']
+        fields = ['name', 'image']
+        
     
 class AddContainerForm(forms.ModelForm):
     class Meta:
         model = Container
-        fields = ['name', 'location', 'no_container']
+        fields = ['name', 'image']
+
 
 class AddLocationForm(forms.ModelForm):
     class Meta:
         model = Location
-        fields = ['name', 'room']
+        fields = ['name']
+    
 
 class AddRoomForm(forms.ModelForm):
     class Meta:
