3b4dc70a5394fda26a3fab69dbea52b1570f7c09
Phil <philconow@gmail.com>
2020-11-10 16:33:47 -0800

    cleaned up extra lines

--- core/views.py
+++ core/views.py
@@ -3,33 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from items.models import Container
 
-# Create your views here.
-
 def frontpage(request):
     containers = Container.objects.all()
-    context = {
-        'containers': containers
-    }
-
-    return render(request, 'frontpage.html', context)
+    return render(request, 'frontpage.html', {'containers': containers})
 
 def about(request):
     return render(request, 'about.html')
 
-
 def contact(request):
     return render(request, 'contact.html')
-
 
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-
         if form.is_valid():
             user = form.save()
             login(request, user) # Logs in and authenticates user
             return redirect('dashboard')
-
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
